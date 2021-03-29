2a210244dba5f0e5f986612a0a1ef01c03a811af
xiao-tang <10761101@gm.nfu.edu.tw>
2021-03-29 17:29:22 +0800

    add index new list

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,16 @@
 class RestaurantsController < ApplicationController
+  #skip_before_action :verify_authenticity_token
   def index
     # render html: 'hello'  #render ({html: 'hello'})
+  end
+
+  def new
+  end
+
+  def list
+    # 寫入資料庫
+    # redirect -> 列表頁
+    # render html: "title is #{params[:title]}"
+    redirect_to '/restaurants'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   get '/restaurants', to: 'restaurants#index'
+
+  get :new_restaurant, to: 'restaurants#new', path: 'ccc'
+
+  post '/restaurant_list', to: 'restaurants#list'
+end
+
   # get '/hello', to: 'a#b'
   # get '/', to: 'a#b'
 
   # get '/about', to: 'pages#about'
   # get '/contact', to: 'pages#contact'
-end
+
